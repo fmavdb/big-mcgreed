b9d2714afc1f8fb66377ffbf7c4e8ec1bde6696d
Frank <Frank@a0cc0eb8-2fcd-7a03-6d30-f3d837ce4de8>
2011-11-23 13:38:41 +0000

    Structuur update.

---
+++ trunk/Big McGreed/Big McGreed/logic/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Big_McGreed.logic
+{
+    public abstract class Entity
+    {
+        //NPC of Speler gebruiken deze gemeenschappelijke class.
+
+
+    }
+}
