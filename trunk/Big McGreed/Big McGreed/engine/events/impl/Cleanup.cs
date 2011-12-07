7b5611190ce298e3f7cca08c982508b65070633f
romedude707@gmail.com <romedude707@gmail.com@a0cc0eb8-2fcd-7a03-6d30-f3d837ce4de8>
2011-12-07 13:13:11 +0000

    -Cleanup.cs added.

---
+++ trunk/Big McGreed/Big McGreed/engine/events/impl/Cleanup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Big_McGreed.engine.events.impl
+{
+    class Cleanup : Event
+    {
+        public Cleanup()
+        {
+            this.delay = 30000; //Every 30 seconds, run memory cleanup.
+        }
+
+        public override void run()
+        {
+            System.GC.Collect();
+        }
+    }
+}
